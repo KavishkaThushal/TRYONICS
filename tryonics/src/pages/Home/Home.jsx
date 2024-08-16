0b51d2375df0d49c8bb35ffa0f244fb10d1e2b9e
kavishka <thushalrasingolla99@gmail.com>
2024-08-16 13:54:53 +0530

    homw is done

--- tryonics/src/pages/Home/Home.jsx
+++ tryonics/src/pages/Home/Home.jsx
@@ -3,7 +3,11 @@ import Card from '../../component/Card/Card'
 
 function Home() {
   return (
-    <div>
+    <div className='grid grid-cols-1 justify-items-center  md:grid-cols-2 lg:grid-cols-4 gap-4 gap-y-10 w-[80%] mx-auto my-10'>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
       <Card/>
     </div>
   )
